92439ba07bfbc362bdff7ec789c7d641563362a1
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2022-11-11 21:30:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+//aboutme
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab");
+}
+    
+//sidemenu
+let sidemeu = document.getElementById("sidemenu")
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px"
+}
+
+
+//contact
